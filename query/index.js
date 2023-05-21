2cfa1cf1ca4e7eff9f3d463813a87ddabfdb4660
Dave <davidmperich@gmail.com>
2023-05-21 08:07:58 -0700

    Add axios to services

--- query/index.js
+++ query/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 //Middleware
 const app = express();
