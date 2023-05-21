f8407fd3a932a2f8a3c408a642ff135249edea89
Dave <davidmperich@gmail.com>
2023-05-21 07:44:13 -0700

    Add cors to services

--- query/index.js
+++ query/index.js
@@ -1,10 +1,12 @@
 //Imports
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 //Middleware
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 //PSUEDO DB
 const cues = {};
