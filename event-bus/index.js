441019989e0d35dc21dfaf8b7166ccf8d8cf6265
Dave <davidmperich@gmail.com>
2023-05-21 13:06:08 -0700

    Finish event-bus service

--- event-bus/index.js
+++ event-bus/index.js
@@ -10,18 +10,24 @@ app.use(bodyParser.json());
 //Psuedo DB
 const events = [];
 
-//Events
+//Send Events To All Services
 app.post('/events', (req, res) => {
     const event = req.body;
-
     events.push(event);
 
-    //TODO: Send Events To Each Service
+    axios.post('http://localhost:4000/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4001/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4002/events', event).catch((err) => {
+        console.log(err.message);
+    });
 
     res.send({ status: 'OK' });
 
 });
-
 
 //Index
 app.get('/events', (req, res) => {
