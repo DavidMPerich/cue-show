9ed11fcb9ba629ecbcee79f4f3acf5aafede2202
Dave <davidmperich@gmail.com>
2023-05-24 17:55:23 -0700

    Create client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Create Cue</h1>
+      <hr />
+      <h1>Posts</h1>
+    </div>
+  );
+}
+
+export default App;
