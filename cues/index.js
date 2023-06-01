5ea6cca82d99908f700b90ff2600ea0dd15d0d5e
Dave <davidmperich@gmail.com>
2023-05-31 17:47:41 -0700

    Add infrastructure functionality

--- cues/index.js
+++ cues/index.js
@@ -23,7 +23,7 @@ app.post('/cues/create', async (req, res) => {
         maker
     }
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'CueCreated',
         data: {
             id,
